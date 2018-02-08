455a7d4aceca7a4a1e464afd98a1cd1e6d9000d9
Monish SP <monishsp@gmail.com>
2018-02-07 20:11:39 -0700

    Blockchain as an API

--- Code/server.py
+++ Code/server.py
@@ -1,7 +1,6 @@
 # Import libraries required
 from uuid import uuid4
-from flask import Flask
-from textwrap import dedent
+from flask import Flask, jsonify, request
 
 # Import locally defined class and functions
 from blockchain import *
@@ -16,13 +15,55 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 # NOTE: Flask is a microframework for mapping endpoints to python functions
+@app.route('/')
+def home():
+    return "Home of custom blockchain"
+
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We run the proof of work algorithm to get the next proof
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    # Response message for mining successfully
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    # Get the data from the POST request
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    # Compose response message and send back response for the request
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # BuildBlockchain
-Using https://github.com/spgitmonish/BuildBlockchain.git to learn
+Using https://hackernoon.com/learn-blockchains-by-building-one-117428612f46 to learn
 about blockchain by building one!
